ccdb16645da2603ce8a109303f619432aadaaab5
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-22 12:51:26 -0500

    Init 2.6

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -21,7 +21,7 @@ class DetailPage extends StatelessWidget {
                 bottomRight: Radius.circular(42.0),
               ),
               image: DecorationImage(
-                image: NetworkImage("https://mymodernmet.com/wp/wp-content/uploads/2019/08/fanjingshan-buddhist-temples-china-7.jpg"),
+                image: NetworkImage("https://images.pexels.com/photos/4215104/pexels-photo-4215104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
                 fit: BoxFit.cover,
               ),
             ),
@@ -29,9 +29,25 @@ class DetailPage extends StatelessWidget {
               children: [
                 Container(
                   decoration: BoxDecoration(
-
+                    borderRadius: const BorderRadius.only(
+                      bottomRight: Radius.circular(42.0),
+                    ),
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.center,
+                      colors: [
+                        Colors.black.withOpacity(0.5),
+                        Colors.transparent,
+                      ],
+                    ),
                   ),
                 ),
+                Column(
+                  children: [
+
+                  ],
+                ),
+
               ],
             ),
           ),
