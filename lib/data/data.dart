f3662f4d73223eb7c4ca3073921e44253eebaca1
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-23 10:44:50 -0500

    Init 4.0

---
+++ lib/data/data.dart
@@ -0,0 +1,23 @@
+
+
+class Data{
+
+  List<Map<String, dynamic>> _data = [
+    {
+      "id": 1,
+      "place": "Santorini",
+      "country": "Greece",
+      "rate": 5,
+      "reviews": 21323,
+      "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+      "overview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "price": 543.50,
+      "days": 2,
+      "image": "https://imageio.forbes.com/specials-images/imageserve/627e31007be5959e6359a3ab/Oia--Santorini-Island--Cyclades--Greece--Twilight--Houses-and-churches-after-sunset/960x0.jpg?format=jpg&width=960"
+    },
+
+
+
+  ];
+
+}
